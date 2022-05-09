1e0eb9166e05302345893fd002be454101a67b81
IoanaMarin1106 <marinvioana@gmail.com>
2022-05-10 01:19:42 +0300

    add ringtone spinner

--- app/src/main/java/com/example/powerconsumptionapp/batteryview/BatterySettingsFragment.kt
+++ app/src/main/java/com/example/powerconsumptionapp/batteryview/BatterySettingsFragment.kt
@@ -1,18 +1,28 @@
 package com.example.powerconsumptionapp.batteryview
 
+import android.content.Context
+import android.media.RingtoneManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.powerconsumptionapp.R
 import com.example.powerconsumptionapp.databinding.FragmentBatterySettingsBinding
-import com.example.powerconsumptionapp.databinding.FragmentBatteryViewBinding
+import com.example.powerconsumptionapp.model.BatteryViewModel
+
 
 class BatterySettingsFragment : Fragment() {
 
     private lateinit var binding: FragmentBatterySettingsBinding
+    private val batteryViewModel: BatteryViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,5 +40,29 @@ class BatterySettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // List available ringtones
+        batteryViewModel.listRingtones(requireActivity())
+
+        // Set spinner list
+        binding.apply {
+            val items = batteryViewModel.ringtonesList.keys.toTypedArray()
+            val adapter:ArrayAdapter<String> = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, items)
+            ringtoneSpinner.adapter = adapter
+            ringtoneSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View?, position: Int, id: Long) {
+                    Toast.makeText(context, "ai ales ${items[position]}", Toast.LENGTH_SHORT).show()
+                    RingtoneManager.setActualDefaultRingtoneUri(
+                        requireContext(),
+                        RingtoneManager.TYPE_ALARM,
+                        Uri.parse(batteryViewModel.ringtonesList[items[position]].toString()));
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                }
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/powerconsumptionapp/model/BatteryViewModel.kt
+++ app/src/main/java/com/example/powerconsumptionapp/model/BatteryViewModel.kt
@@ -1,5 +1,11 @@
 package com.example.powerconsumptionapp.model
 
+import android.database.Cursor
+import android.media.RingtoneManager
+import android.net.Uri
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.powerconsumptionapp.R
@@ -9,6 +15,8 @@ import com.example.powerconsumptionapp.startfragment.buttonsList
 import kotlinx.coroutines.launch
 
 class BatteryViewModel: ViewModel() {
+    var ringtonesList: HashMap<String, Uri> = HashMap()
+
     fun populateFragmentButtons() {
         viewModelScope.launch {
             if (buttonsList.isEmpty()) {
@@ -22,6 +30,18 @@ class BatteryViewModel: ViewModel() {
                     add(performanceManagerBttn)
                 }
             }
+        }
+    }
+
+    fun listRingtones(activity: FragmentActivity) {
+        val manager = RingtoneManager(activity)
+        manager.setType(RingtoneManager.TYPE_RINGTONE)
+        val cursor: Cursor = manager.cursor
+        while (cursor.moveToNext()) {
+            val title: String = cursor.getString(RingtoneManager.TITLE_COLUMN_INDEX)
+            val ringtoneURI: Uri = manager.getRingtoneUri(cursor.position)
+            // Do something with the title and the URI of ringtone
+            ringtonesList[title] = ringtoneURI
         }
     }
 }
