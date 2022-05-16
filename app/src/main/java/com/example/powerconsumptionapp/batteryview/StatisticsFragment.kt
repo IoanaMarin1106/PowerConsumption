a5da9270b3a8bbd885d2468a85726214b4027012
IoanaMarin1106 <marinvioana@gmail.com>
2022-05-16 16:06:40 +0300

    add graph view -> x axis & percent y axis

--- app/src/main/java/com/example/powerconsumptionapp/batteryview/StatisticsFragment.kt
+++ app/src/main/java/com/example/powerconsumptionapp/batteryview/StatisticsFragment.kt
@@ -1,21 +1,27 @@
 package com.example.powerconsumptionapp.batteryview
 
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
-import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewParent
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.powerconsumptionapp.R
 import com.example.powerconsumptionapp.databinding.FragmentStatisticsBinding
+import com.jjoe64.graphview.DefaultLabelFormatter
 import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.Viewport
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
-import kotlin.random.Random
+import java.text.Format
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class StatisticsFragment : Fragment() {
@@ -45,26 +51,49 @@ class StatisticsFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val graph = binding.batteryGraph
-        // data
-        series = LineGraphSeries(arrayOf(
-            DataPoint(0.0, 1.0),
-            DataPoint(1.0, 5.0),
-            DataPoint(2.0, 3.0),
-        ))
+        val graph = binding.batteryGraph as GraphView
+        series = LineGraphSeries(
+            arrayOf(
+                DataPoint(Date(), 25.0),
+                DataPoint(Date(), 30.0),
+                DataPoint(Date(), 20.0),
+                DataPoint(Date(), 60.0),
+                DataPoint(Date(), 60.0),
+            )
+        )
         graph.addSeries(series)
+        graph.gridLabelRenderer.numHorizontalLabels = 3
 
-        //customize graph view
+        graph.gridLabelRenderer.labelFormatter = object : DefaultLabelFormatter() {
+            override fun formatLabel(value: Double, isValueX: Boolean): String {
+                if (isValueX) {
+                    val sdf = SimpleDateFormat("hh:mm:ss")
+                    return sdf.format(value)
+                } else {
+                    return super.formatLabel(value, isValueX) + "%"
+                }
+            }
+        }
+
+        // customize graph view
         var viewport: Viewport = graph.viewport
-        viewport.setMinX(0.0)
-        viewport.setMaxX(10.0)
-        viewport.isXAxisBoundsManual = true
         viewport.isScalable = true
+        viewport.setScalableY(true)
+        viewport.isYAxisBoundsManual = true
+        viewport.setMinY(0.0)
+        viewport.setMaxY(100.0)
 
-        addDataPoint()
+//        addDataPoint()
+
+        binding.hourButton.setOnClickListener {
+            val sdf = SimpleDateFormat("hh:mm")
+            val currentDate = sdf.format(Date())
+            Toast.makeText(context, currentDate.toString(), Toast.LENGTH_SHORT).show()
+        }
 
         val graph2 = binding.batteryGraph1 as GraphView
         val series2: LineGraphSeries<DataPoint> = LineGraphSeries(
@@ -79,12 +108,12 @@ class StatisticsFragment : Fragment() {
         graph2.addSeries(series2)
     }
 
-    //add data to the graph
-    private fun addDataPoint() {
-       handler.postDelayed({
-           lastX += 0.5
-           series.appendData(DataPoint(lastX, 4.0), false, 100)
-           addDataPoint()
-       }, 1000)
-    }
+//    //add data to the graph
+//    private fun addDataPoint() {
+//       handler.postDelayed({
+//           lastX += 0.5
+//           series.appendData(DataPoint(lastX, 4.0), false, 100)
+//           addDataPoint()
+//       }, 1000)
+//    }
 }
